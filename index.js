e0edf3285a65bcb7a70e1ffff8e712e4eb939f47
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2015-05-30 14:42:51 -0400

    Throw on no aws and set credentials in config

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 module.exports = function(aws, profile, env){
   var credentials;
-
+  
+  if(!aws || !aws.EC2 || !aws.S3){
+    throw new Error('Must pass an instance of the aws sdk as the first argument.'); 
+  }
   if(typeof profile === 'object' && !env){
     env = profile;
     profile = 'default';
@@ -14,6 +17,8 @@ module.exports = function(aws, profile, env){
   }else{
     credentials = new aws.SharedIniFileCredentials({profile: profile});
   }
+  
+  aws.config.credentials = credentials;
 
   return credentials;
 };

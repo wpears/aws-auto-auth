8f473b5a7cb420eb405ca02fd9798377acdffcf8
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2015-05-31 13:38:30 -0400

    obj -> env -> profile tested

--- test/test.js
+++ test/test.js
@@ -4,31 +4,85 @@ var awsAutoAuth = require('../index');
 
 test('With profile', function(t){
   t.plan(2);
+  
+  process.env.AWS_ACCESS_KEY_ID = '';
+  process.env.AWS_SECRET_ACCESS_KEY = '';
+
   var cred = awsAutoAuth(aws, 'default');
   t.ok(cred, 'Makes credentials.'); 
   t.ok(cred.accessKeyId, 'Picks up access key.')
 });
 
-test('With obj', function(t){
+test('With credentials object', function(t){
   t.plan(2);
+
+  process.env.AWS_ACCESS_KEY_ID = '';
+  process.env.AWS_SECRET_ACCESS_KEY = '';
+
   var cred = awsAutoAuth(aws, {accessKeyId: 'fake', secretAccessKey:'faker'});
-  console.log(cred);
   t.ok(cred, 'Makes credentials.');
-  t.equal(cred.accessKeyId, 'fake', 'Credentials from obj.');
+  t.equal(cred.accessKeyId, 'fake', 'Credentials from object.');
+});
+
+test('With env', function(t){
+  t.plan(2);
+
+  process.env.AWS_ACCESS_KEY_ID = 'envkey';
+  process.env.AWS_SECRET_ACCESS_KEY = 'secret';
+
+  var cred = awsAutoAuth(aws);
+  t.ok(cred, 'Makes credentials.'); 
+  t.equal(cred.accessKeyId, 'envkey', 'Pulls credentials from environmental variables.')
+
+  process.env.AWS_ACCESS_KEY_ID = '';
+  process.env.AWS_SECRET_ACCESS_KEY = '';
 });
 
 test('With profile and obj', function(t){
   t.plan(2);
+
+  process.env.AWS_ACCESS_KEY_ID = '';
+  process.env.AWS_SECRET_ACCESS_KEY = '';
+
   var cred = awsAutoAuth(aws, 'default', {accessKeyId: 'fake', secretAccessKey:'faker'});
   t.ok(cred, 'Makes credentials.');
   t.equal(cred.accessKeyId, 'fake', 'Credentials from obj override profile.');
 });
 
+test('With profile and env', function(t){
+  t.plan(2);
+
+  process.env.AWS_ACCESS_KEY_ID = 'envkey';
+  process.env.AWS_SECRET_ACCESS_KEY = 'secret';
+
+  var cred = awsAutoAuth(aws, 'default');
+  t.ok(cred, 'Makes credentials.'); 
+  t.equal(cred.accessKeyId, 'envkey', 'Pulls credentials from environmental variables over profile.')
+
+  process.env.AWS_ACCESS_KEY_ID = '';
+  process.env.AWS_SECRET_ACCESS_KEY = '';
+});
+
+test('With profile, env, and obj', function(t){
+  t.plan(2);
+  process.env.AWS_ACCESS_KEY_ID = 'envkey';
+  process.env.AWS_SECRET_ACCESS_KEY = 'secret';
+
+  var cred = awsAutoAuth(aws, 'default', {accessKeyId: 'fake', secretAccessKey:'faker'});
+  t.ok(cred, 'Makes credentials.'); 
+  t.equal(cred.accessKeyId, 'fake', 'Passed credentials obj overrides env and profile.')
+
+  process.env.AWS_ACCESS_KEY_ID = '';
+  process.env.AWS_SECRET_ACCESS_KEY = '';
+});
+
 test('Only aws', function(t){
- t.plan(3); 
+  t.plan(2); 
+
+  process.env.AWS_ACCESS_KEY_ID = '';
+  process.env.AWS_SECRET_ACCESS_KEY = '';
+
   var cred = awsAutoAuth(aws);
-  console.log(cred);
   t.ok(cred, 'Makes credentials.');
   t.ok(cred.accessKeyId, 'Credentials from profile.');
-  t.notEqual(cred.accessKeyId, 'fake', 'Credentials not from env.');
 });
